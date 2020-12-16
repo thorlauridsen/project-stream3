7f218f31b192014a521c89323c3ee7a8fd14c91b
REXKrash <rexkrash@yahoo.dk>
2020-12-16 10:12:19 +0100

    Screen size update
    Screen size update

--- src/com/stream/controllers/PageController.java
+++ src/com/stream/controllers/PageController.java
@@ -38,6 +38,8 @@ public class PageController {
         frame.getContentPane().removeAll();
         frame.add(component);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setMinimumSize(new Dimension(800, 600));
+        frame.setPreferredSize(new Dimension(1920, 1000));
         frame.setVisible(true);
         frame.revalidate();
         frame.repaint();
